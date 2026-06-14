b5f9602b360daec6bac26a09240a5f8e1efdd837
CodingalReyansh <acharya.nidhi28@gmail.com>
2026-06-14 11:48:06 -0400

    Printing statements

---
+++ Printing.py
@@ -0,0 +1,8 @@
+# Greeting statement
+print ("Welcome to Codingal")
+# My Name
+print ("My name is Reyansh")
+# Addition of 2 numbers
+print ("Addition of 3 and 5 is",3+5) 
+# Multiplication of 2 numbers
+print ("Multiplication os 3 and 5 is", 3*5)
